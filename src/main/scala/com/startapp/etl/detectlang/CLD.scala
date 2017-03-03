214a5ab24ee78625caeea27b1b2a99015ad0263c
Sid Feiner <sidney.feiner@startapp.com>
2017-03-03 18:02:14 +0200

    Adapt scala CLD to new changed

--- src/main/scala/com/startapp/etl/detectlang/CLD.scala
+++ src/main/scala/com/startapp/etl/detectlang/CLD.scala
@@ -19,7 +19,9 @@ object CLD {
         // getProbableLocale returns a java.util.Locale
         Some(result.getProbableLocale.toLanguageTag)
       } else {
-        Some(result.getCandidates.asScala.sortBy(_.getScore).last.getLocale.toLanguageTag)
+        val candidates = result.getCandidates
+        if (candidates == null) None
+        else Some(candidates.asScala.head.getLocale.toLanguageTag)
       }
     }
   }
